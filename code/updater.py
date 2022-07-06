1bd332cbcd5b4425d8fd88679b52ba15103944ca
username121546434 <gokuleatharv06@gmail.com>
2022-07-06 12:42:51 -0400

    started working on a separate updater

--- code/updater.py
+++ code/updater.py
@@ -3,9 +3,7 @@ import sys
 import requests
 from packaging.version import Version
 from tkinter import *
-from tkinter.ttk import *
 from tkinter import messagebox
-from zipfile import ZipFile
 from markdown import Markdown
 from io import StringIO
 
@@ -61,43 +59,8 @@ If you do have an internet connection, then here are details so you can report a
 
     if Version(latest_version) > Version(current_version) and choice.get():
         messagebox.showinfo(title='Update', message='Press "Ok" to update Minesweeper')
-        if download_zip:
-            asset = [
-                asset
-                for asset in response.json()[0]['assets']
-                if asset['content_type'] == 'application/x-zip-compressed'
-            ][0]
-        else:
-            asset = [
-                asset
-                for asset in response.json()[0]['assets']
-                if asset['content_type'] == 'application/x-msdownload'
-            ][0]
-        download_url = asset['browser_download_url']
-        filename = asset['name']
-        progress_window = Toplevel(master)
-        progress_window.title('Updater')
-        download = requests.get(download_url, stream=True)
-        progress = Progressbar(progress_window, mode='determinate', length=200)
-        progress.pack()
-        total_size = int(download.headers.get('content-length'))
-        increment = (1 / total_size) * 100
-        with open(os.path.expanduser(f'~\\Downloads\\{filename}'), 'wb') as f:
-            for data in download.iter_content(chunk_size=1000):
-                progress['value'] += increment * len(data)
-                f.write(data)
-                progress_window.update()
-        progress_window.destroy()
-        if download_zip:
-            with ZipFile(os.path.expanduser(f'~\\Downloads\\{filename}')) as zip_file:
-                dir = filename[:-3]
-                dir = os.path.expanduser(f'~\\Downloads\\{dir}')
-                zip_file.extractall(dir)
-            messagebox.showinfo(title='Update', message=f'''The update has completed, you can find it in {dir}. You can delete the previous update or keep it in case you get issues with the new one.''')
-        else:
-            master.destroy()
-            os.startfile(os.path.expanduser(f'~\\Downloads\\{filename}'))
-            sys.exit()
+        os.startfile('updater.exe')
+        sys.exit()
     else:
         try:
             _ = choice
