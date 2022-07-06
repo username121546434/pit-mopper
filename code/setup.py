1a59dea24ff115551ccb62fcb010d1b0a9ed231b
username121546434 <gokuleatharv06@gmail.com>
2022-07-06 14:45:06 -0400

    formatted

--- code/setup.py
+++ code/setup.py
@@ -2,7 +2,12 @@ from cx_Freeze import setup, Executable
 
 base = None    
 
-executables = [Executable("./minesweeper-python/code/main.py", base='Win32GUI', icon='./minesweeper-python/code/data/images/logo.ico')]
+executables = [Executable(
+    "./minesweeper-python/code/main.py",
+    base='Win32GUI',
+    icon='./minesweeper-python/code/data/images/logo.ico',
+    copyright='GNU GPL v3, see LICENSE.txt for more info'
+    )]
 
 packages = [
     "functools",
