0ead3098619dd16dc052c1c30c39146c7634d95f
username121546434 <gokuleatharv06@gmail.com>
2022-09-04 17:32:47 -0400

    change keyboard shortcut for quiting app

--- Scripts/enums.py
+++ Scripts/enums.py
@@ -4,7 +4,6 @@ from enum import Enum
 class KBDShortcuts(Enum):
     """Contains all of the keyboard shorcuts used by Pit Mopper"""
     toggle_theme       =   '<Control-d>'
-    quit_app           =   '<Control-q>'
     check_for_updates  =   '<Control-u>'
     version_info       =   '<Control-i>'
     toggle_console     =   '<Control-x>'
@@ -15,5 +14,6 @@ class KBDShortcuts(Enum):
     save_file          =   '<Control-s>'
     start_game         =   '<space>'
     quit_game          =   '<Alt-q>'
+    quit_app           =   '<Alt-F4>'
     game_info          =   '<Alt-i>'
     fullscreen         =   '<F11>'
