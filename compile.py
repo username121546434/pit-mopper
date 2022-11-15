386cda3c913a53662d68bc956c0233c6275ab7e3
username121546434 <gokuleatharv06@gmail.com>
2022-11-14 21:33:41 -0500

    updated compile.py

--- compile.py
+++ compile.py
@@ -6,40 +6,37 @@ from cx_Freeze import setup, Executable
 base = 'Win32GUI' if os.name == 'nt' else None
 icon = LOGO if os.name == 'nt' else LOGO[1:]
 
-executables = [Executable(
-    "./Pit Mopper.py",
-    base=base,
-    icon=icon,
-    copyright='GNU GPL v3, see LICENSE.txt for more info'
-)]
-
-packages = [
-    'html.parser' # Required by the markdown library
-]
-
 if os.name == 'nt':
+    executables = [Executable(
+        "./Pit Mopper.py",
+        base=base,
+        icon=icon,
+        copyright='GNU GPL v3, see LICENSE.txt for more info'
+    )]
+
+    packages = [
+        'html.parser' # Required by the markdown library
+    ]
+
     include_files = [
         ('./Scripts/data/fonts', 'data/fonts'),
         ('./Scripts/data/images/windows_icon.ico', 'data/images/windows_icon.ico'),
         ('./Scripts/data/images/windows_icon_darkmode.ico', 'data/images/windows_icon_darkmode.ico'),
     ]
+
+    options = {
+        'build_exe': {
+            'packages':packages,
+            'include_files':include_files
+        },
+    }
+
+    setup(
+        name = "Pit Mopper",
+        options = options,
+        version = VERSION,
+        description = '',
+        executables = executables
+    ) 
 else:
-    include_files = [
-        ('./Scripts/data/images/linux_logo.xbm', 'data/images/linux_logo.xbm'),
-        ('./Scripts/data/images/linux_logo_darkmode.xbm', 'data/images/linux_logo_darkmode.xbm')
-    ]
-
-options = {
-    'build_exe': {
-        'packages':packages,
-        'include_files':include_files
-    },
-}
-
-setup(
-    name = "Pit Mopper",
-    options = options,
-    version = VERSION,
-    description = '',
-    executables = executables
-) 
+    os.system('pyinstaller --add-data "./data/images/linux_icon_darkmode.xbm:data/images/linux_icon_darkmode.xbm" --add-data "data/images/linux_icon.xbm:data/images/linux_icon.xbm" -i data/images/linux_icon.xbm -D -w "./Pit Mopper.py"')
