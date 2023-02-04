4317e6f351011110c76405bed2e396787c3928e4
username121546434 <gokuleatharv06@gmail.com>
2023-02-03 19:42:57 -0500

    made the updater work on ubuntu

---
+++ Scripts/prog_bar_tk.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import os
+from tkinter.ttk import Progressbar
+from typing import Iterable, TypeVar
+from .functions import change_theme_of_window
+
+T = TypeVar('T')
+
+
+def progress_bar_window(iterable: Iterable[T], total_size: int, text_format: str='Loading... {}', title: str='Loading...') -> Iterable[T]:
+    """Creates a new tkinter `Toplevel` and creates a progress bar for a task
+
+    Args:
+        `iterable`: An iterable to use as a task
+        `total_size`: The total length of the iterable
+        `text_format`: A format string that will be used for the label
+        `title`: The title to be used for the window
+
+    Returns: An iterator that will automatically update the progress in the loopp
+    """
+    progress_window = Toplevel()
+    progress_window.config(padx=20, pady=20)
+    progress_window.title(title)
+    if os.name == 'nt':
+        progress_window.master.attributes('-disabled', True)
+        progress_window.attributes('-disabled', True)
+
+    label = Label(progress_window, text=text_format.format('0%'))
+    label.pack()
+    progress = Progressbar(progress_window, mode='determinate', length=200)
+    progress.pack()
+
+    increment = (1 / total_size) * 100
+    percent = 0.0
+
+    change_theme_of_window(progress_window)
+
+    for data in iterable:
+        progress['value'] += increment * len(data)
+        percent += len(data) / total_size
+        label.config(text=text_format.format(f'{percent:.2%}'))
+        progress_window.update()
+        yield data
+
+    progress_window.destroy()
--- Scripts/updater.py
+++ Scripts/updater.py
@@ -1,32 +1,146 @@
+import glob
 import os
 import subprocess
 import sys
 import webbrowser
 import requests
 from packaging.version import Version
-from tkinter import Toplevel, BooleanVar, Checkbutton, Button, Label
-from tkinter.ttk import Progressbar
-from zipfile import ZipFile
+from tkinter import Toplevel, BooleanVar, Checkbutton, Button
 from markdown import markdown
 from tkinterweb.htmlwidgets import HtmlFrame
 import tkinter.messagebox as messagebox
+
+from .prog_bar_tk import progress_bar_window
 from .constants import VERSION
 from .network import check_internet
+import tempfile
+import shutil
 from .functions import change_theme_of_window
+
+
+def is_new_update_available() -> tuple[bool, dict]:
+    """
+    Checks if an update is available
+
+    Returns: A tuple where the first element is whether there is and update available, and the second is the version info
+    """
+    response = requests.get('https://api.github.com/repos/username121546434/pit-mopper/releases/latest')
+    response.raise_for_status()
+    latest_version_info = response.json()
+    latest_version = latest_version_info["tag_name"]
+
+    return (Version(latest_version) > Version(VERSION), latest_version_info)
+
+
+def download(download_url: str, extension: str | None=None) -> str:
+    """
+    Downloads a file from `download_url` and saves it as a temporary file
+
+    Args:
+        `download_url`: The url to download the file from
+        `extension`: The file extension of the file
+
+    Returns: The path to the file where it was written
+    """
+    download = requests.get(download_url, stream=True)
+    total_size = int(download.headers.get('content-length'))
+
+    filename = tempfile.mkstemp(suffix=extension)
+    f = os.fdopen(filename[0], 'wb')
+
+    try:
+        for data in progress_bar_window(download.iter_content(chunk_size=1_000), total_size, 'Downloading... {}'):
+            f.write(data)
+    finally:
+        f.close()
+    return filename[1]
+
+
+def update_windows(app_quit, assets: list):
+    messagebox.showinfo(title='Update', message='Press "Ok" to update Pit Mopper')
+    download_zip = not os.path.exists('unins0000.exe')
+
+    if download_zip:
+        asset = [
+            asset
+            for asset in assets
+            if asset['content_type'] == 'application/x-zip-compressed' # *.zip file
+        ][0]
+    else:
+        asset = [
+            asset
+            for asset in assets
+            if asset['content_type'] == 'application/x-msdownload' # *.exe file
+        ][0]
+    download_url: str = asset['browser_download_url']
+    filename: str = asset['name']
+    file_extension = '.' + filename.split('.')[-1]
+
+    installer_or_zip_file = download(download_url, file_extension)
+
+    bat_file = os.path.join(os.getenv('TEMP'), 'Pit Mopper Update.bat')
+    if download_zip:
+        extract_dir = tempfile.mkdtemp()
+        shutil.unpack_archive(installer_or_zip_file, extract_dir, 'zip')
+        with open(bat_file, 'w') as bat:
+            bat.write(f'''
+@echo off
+timeout 3 /NOBREAK
+del /q "{installer_or_zip_file}"
+robocopy /MOV /MIR {extract_dir} "{os.getcwd()}"
+rmdir {extract_dir} /S /Q
+start "Pit Mopper" "{os.path.join(os.path.split(sys.executable)[0], 'Pit Mopper.exe')}"
+(goto) 2>nul & del "%~f0"
+''')
+    else:
+        with open(bat_file, 'w') as bat:
+            bat.write(f'''
+@echo off
+timeout 3 /NOBREAK
+"{installer_or_zip_file}" /SILENT /SUPPRESSMSGBOXES /NOCANCEL /FORCECLOSEAPPLICATIONS
+del /q "{installer_or_zip_file}"
+start "Pit Mopper" "{os.path.join(os.path.split(sys.executable)[0], 'Pit Mopper.exe')}"
+(goto) 2>nul & del "%~f0"
+''')
+    subprocess.Popen(['cmd', '/C', bat_file], creationflags=subprocess.CREATE_NO_WINDOW,
+                start_new_session=True)
+    app_quit()
+
+
+def update_ubuntu(app_quit, assets: list):
+    messagebox.showinfo(title='Update', message='Press "Ok" to update Pit Mopper')
+
+    asset = [
+        asset
+        for asset in assets
+        if asset['name'].endswith('.tar.gz') and 'Ubuntu' in asset['name']
+    ][0]
+    download_url: str = asset['browser_download_url']
+
+    file = download(download_url, '.tar.gz')
+    curr_dir = os.getcwd()
+    tmp_dir = tempfile.mkdtemp()
+    extract_dir = os.path.join(tmp_dir, os.path.basename(curr_dir))
+    os.mkdir(extract_dir)
+
+    shutil.unpack_archive(file, extract_dir, 'gztar')
+    os.remove(file)
+    shutil.rmtree(curr_dir)
+    shutil.move(extract_dir, os.path.split(curr_dir)[0])
+    shutil.rmtree(tmp_dir)
+
+    app_quit()
 
 
 def check_for_updates(app_quit):
     if not check_internet():
         messagebox.showerror('No internet', 'You can only check for updates with an internet connection')
         return
-    response = requests.get('https://api.github.com/repos/username121546434/pit-mopper/releases/latest')
-    response.raise_for_status()
-    latest_version_info = response.json()
-    latest_version = latest_version_info["tag_name"]
+    update_available, latest_version_info = is_new_update_available()
     body = latest_version_info['body']
     title = latest_version_info['name']
 
-    if Version(latest_version) > Version(VERSION):
+    if update_available:
         window = Toplevel()
         window.title('Update Available')
         choice = BooleanVar(window)
@@ -48,81 +162,9 @@ def check_for_updates(app_quit):
         return
     
     if os.name == 'posix' and choice.get():
-        messagebox.showinfo('Update', 'Automatic updates are not supported on your platform, but you will be taken to the download link')
-        webbrowser.open(f'https://github.com/username121546434/pit-mopper/releases/tag/{latest_version}/')
-
+        update_ubuntu(app_quit, latest_version_info['assets'])
     elif os.name == 'nt' and choice.get():
-        messagebox.showinfo(title='Update', message='Press "Ok" to update Pit Mopper')
-        download_zip = not os.path.exists('unins0000.exe')
-
-        if download_zip:
-            asset = [
-                asset
-                for asset in latest_version_info['assets']
-                if asset['content_type'] == 'application/x-zip-compressed' # *.zip file
-            ][0]
-        else:
-            asset = [
-                asset
-                for asset in latest_version_info['assets']
-                if asset['content_type'] == 'application/x-msdownload' # *.exe file
-            ][0]
-        download_url: str = asset['browser_download_url']
-        filename: str = asset['name']
-        progress_window = Toplevel()
-        progress_window.config(padx=20, pady=20)
-        progress_window.title('Updater')
-        progress_window.master.attributes('-disabled', True)
-        progress_window.attributes('-disabled', True)
-        download = requests.get(download_url, stream=True)
-
-        label = Label(progress_window, text='Downloading... 0%')
-        label.pack()
-        progress = Progressbar(progress_window, mode='determinate', length=200)
-        progress.pack()
-
-        installer_or_zip_file = os.path.join(os.getenv('TEMP'), filename)
-        total_size = int(download.headers.get('content-length'))
-        increment = (1 / total_size) * 100
-        percent = 0.0
-
-        change_theme_of_window(progress_window)
-
-        with open(installer_or_zip_file, 'wb') as f:
-            for data in download.iter_content(chunk_size=1000):
-                progress['value'] += increment * len(data)
-                percent += len(data) / total_size
-                label.config(text=f'Downloading... {percent:.2%}')
-                f.write(data)
-                progress_window.update()
-
-        progress_window.destroy()
-
-        bat_file = os.path.join(os.getenv('TEMP'), 'Pit Mopper Update.bat')
-        if download_zip:
-            with ZipFile(installer_or_zip_file) as zip_file:
-                directory = filename.split('.')[0]
-                directory = os.path.join(os.getenv('TEMP'), directory)
-                zip_file.extractall(directory)
-            os.remove(installer_or_zip_file)
-            with open(bat_file, 'w') as bat:
-                bat.write(f'''@echo off
-timeout 3 /NOBREAK
-robocopy /MOV /MIR {directory} "{os.getcwd()}"
-rmdir {directory} /S /Q
-start "Pit Mopper" "{os.path.join(os.path.split(sys.executable)[0], 'Pit Mopper.exe')}"
-(goto) 2>nul & del "%~f0"''')
-        else:
-            with open(bat_file, 'w') as bat:
-                bat.write(f'''@echo off
-timeout 3 /NOBREAK
-"{installer_or_zip_file}" /SILENT /SUPPRESSMSGBOXES /NOCANCEL /FORCECLOSEAPPLICATIONS
-del /q "{installer_or_zip_file}"
-start "Pit Mopper" "{os.path.join(os.path.split(sys.executable)[0], 'Pit Mopper.exe')}"
-(goto) 2>nul & del "%~f0"''')
-        subprocess.Popen(['cmd', '/C', bat_file], creationflags=subprocess.CREATE_NO_WINDOW,
-                 start_new_session=True)
-        app_quit()
+        update_windows(app_quit, latest_version_info['assets'])
     else:
         messagebox.showinfo('Update Rejected', 'You have rejected the update')
         return
